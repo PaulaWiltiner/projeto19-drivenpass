97a258f95aa5fd23f303aaf215fdb3e70f7f1869
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-09-08 19:55:13 -0300

    credentials

--- src/services/credentialService.ts
+++ src/services/credentialService.ts
@@ -27,18 +27,38 @@ export async function createCredential(url:string,name:string,newPassword:string
  export async function getCredentialId(id:number,token:string){
   const userId=await authenticateToken(token);
   const result= await findById(id);
+
   if(!result){
     throw {code:'NotFound' , message:'Credential not found'}
   }
   if(userId!==result.userId){
     throw {code:'Unathorized' , message:'user unathorized'}
   }
+  const cryptr = new Cryptr('cardTotallySecretKey');
+  const decryptedPass = cryptr.decrypt(result.password);
+  result["password"] = decryptedPass ;
+      
+
   return result
  }
 
  export async function getCredentials(token:string){
   const userId=await authenticateToken(token);
   const result = await find(userId);
+  const response = result.map((item:any) => {
+    item["customer"] = {
+      id: Number(listCustomer[0]),
+      name: listCustomer[1],
+    };
+    const listGame = item.game.replace(/[\\"()]/g, "").split(",", 4);
+    item["game"] = {
+      id: Number(listGame[0]),
+      name: listGame[1],
+      categoryId: Number(listGame[2]),
+      categoryName: listGame[3],
+    };
+    return item;
+  });
   return result
  }
 
