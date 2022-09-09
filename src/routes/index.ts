079c5fa448123e56aa428c2163b05c7794661cfa
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-09-09 11:19:56 -0300

    wifis

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import authRouter from "./authRouter";
 import credentialsRouter from "./credentialsRouter";
 import securityNotesRouter from "./securityNotesRoutes";
+import WifisRouter from "./wifisRouter";
 
 const router = Router();
 router.use(authRouter);
 router.use(credentialsRouter);
 router.use(securityNotesRouter);
+router.use(WifisRouter);
 
 export default router;
