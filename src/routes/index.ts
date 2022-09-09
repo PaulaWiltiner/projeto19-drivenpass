76a6f04625fc0aed45ee4eefc1f67c9ed52f8214
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-09-08 21:35:01 -0300

    securityNotes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./authRouter";
 import credentialsRouter from "./credentialsRouter";
+import securityNotesRouter from "./securityNotesRoutes";
 
 const router = Router();
 router.use(authRouter);
 router.use(credentialsRouter);
+router.use(securityNotesRouter);
 
 export default router;
